68d4a1cf54fac1584e8981c687e72cd97604fefb
Michael McEniry [UAH] <mcenirm@uah.edu>
2019-03-17 17:26:04 -0500

    Ensure UTC timezone

--- scripts/1-setup-system.bash
+++ scripts/1-setup-system.bash
@@ -1,4 +1,15 @@
 #!/bin/bash
 set -eu
 
+echo = Update package cache
 sudo apt-get update
+echo
+
+echo = Ensure UTC timezone
+# Note: Vagrant and AWS EC2 use UTC by default
+if [ "$(date +%Z)" != UTC ]
+then
+  cat Etc/UTC | sudo tee /etc/timezone >/dev/null
+  sudo dpkg-reconfigure -f noninteractive tzdata
+fi
+echo
