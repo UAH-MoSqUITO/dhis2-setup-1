b7a1abb229cf8671647a3b56326828f981652323
Michael McEniry [UAH] <mcenirm@uah.edu>
2019-03-17 18:31:18 -0500

    Generate locale

--- scripts/1-setup-system.bash
+++ scripts/1-setup-system.bash
@@ -1,15 +1,35 @@
 #!/bin/bash
 set -eu
 
+
+# Variables
+# TODO put these in a separate file
+
+tztest=UTC
+tzconf=Etc/UTC
+
+localetest=en_US.utf8
+localeconf=en_US.UTF-8
+
+
+# Setup
+
 echo = Update package cache
 sudo apt-get update
 echo
 
-echo = Ensure UTC timezone
+echo = Ensure timezone
 # Note: Vagrant and AWS EC2 use UTC by default
-if [ "$(date +%Z)" != UTC ]
+if [ "$(date +%Z)" != "$tztest" ]
 then
-  cat Etc/UTC | sudo tee /etc/timezone >/dev/null
+  printf '%s\n' "$tzconf" | sudo tee /etc/timezone >/dev/null
   sudo dpkg-reconfigure -f noninteractive tzdata
+fi
+echo
+
+echo = Generate locale
+if ! localedef --list | grep -qxF -e "$localetest"
+then
+  sudo locale-gen "$localeconf"
 fi
 echo
